c9f7f1052f549c69db7e52ecff2a4bdd450ff212
Gustavo Adolfo Rodriguez Libreros <1432287+garodriguezlp@users.noreply.github.com>
2022-07-30 17:26:10 -0500

    Initial version of script

---
+++ payroll.java
@@ -0,0 +1,28 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS info.picocli:picocli:4.5.0
+
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "payroll", mixinStandardHelpOptions = true, version = "payroll 0.1",
+        description = "payroll made with jbang")
+class payroll implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
+    private String greeting;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new payroll()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.out.println("Hello " + greeting);
+        return 0;
+    }
+}
