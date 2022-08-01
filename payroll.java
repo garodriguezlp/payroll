3c9c259e5c47af803e526b4b127664cc69ceeb21
Gustavo Adolfo Rodriguez Libreros <1432287+garodriguezlp@users.noreply.github.com>
2022-08-01 03:38:43 -0500

    Add implementation to parse most of the fields

--- payroll.java
+++ payroll.java
@@ -2,7 +2,10 @@
 //DEPS info.picocli:picocli:4.5.0
 //DEPS org.apache.pdfbox:pdfbox:2.0.26
 //DEPS org.apache.commons:commons-csv:1.9.0
+//DEPS org.projectlombok:lombok:1.18.24
 
+import lombok.Builder;
+import lombok.Value;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -23,6 +26,8 @@ import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.apache.commons.csv.CSVFormat.TDF;
+
 @Command(name = "payroll", mixinStandardHelpOptions = true, version = "payroll 0.1",
         description = "payroll made with jbang")
 class payroll implements Callable<Integer> {
@@ -30,13 +35,30 @@ class payroll implements Callable<Integer> {
     private static final NumberFormat NUMBER_FORMAT = NumberFormat.getNumberInstance(Locale.US);
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    private static final Pattern DATE_PATTERN = Pattern.compile("Fecha cpbte: ((\\d{2}\\/){2}(\\d{4}))");
-    private static final String NUMBER_REGEX = "((\\d*,)*(\\d*).(\\d*))";
-    private static final Pattern RETENTION_PATTERN = Pattern.compile("RETENCION EN LA FUENTE - {2}" + NUMBER_REGEX);
+    private static final int SECOND_MATCHING_GROUP = 2;
+    private static final int FIRST_MATCHING_GROUP = 1;
+
+    private static final Pattern DATE_PATTERN = Pattern.compile("Fecha cpbte: ((\\d{2}/){2}(\\d{4}))");
+    private static final String NUMBER_REGEX = "((\\d{1,3},){1,3}(\\d*).(\\d*))";
+    private static final Pattern WORK_HOURS_PATTERN = Pattern.compile("laboradas: {2}" + "((\\d{1,3}).(\\d*))");
     private static final Pattern WAGE_PATTERN = Pattern.compile("Salario: {2}" + NUMBER_REGEX);
 
-    private static final CSVFormat CSV_FORMAT = CSVFormat.Builder.create()
-            .setHeader("Date", "Wage", "Retention")
+    private static final Pattern PERIOD_WAGE_PATTERN = Pattern.compile("(SALARIO BASICO|S.I. INTEGRAL) \\+ {2}" + NUMBER_REGEX);
+    private static final Pattern VACATIONS_PATTERN = Pattern.compile("VACACIONES \\+ {2}" + NUMBER_REGEX);
+    private static final Pattern TOTAL_WAGE_PATTERN = Pattern.compile("TOTAL DEVENGADOS {2}" + NUMBER_REGEX);
+
+    private static final Pattern RETENTION_PATTERN = Pattern.compile("RETENCION EN LA FUENTE - {2}" + NUMBER_REGEX);
+    private static final Pattern HEALTH_PATTERN = Pattern.compile("DEDUCCION SALUD - {2}" + NUMBER_REGEX);
+    private static final Pattern PENSION_PATTERN = Pattern.compile("DEDUCCION PENSION - {2}" + NUMBER_REGEX);
+    private static final Pattern SOLIDARITY_PATTERN = Pattern.compile("FONDO DE SOLIDARIDAD - {2}" + NUMBER_REGEX);
+    private static final Pattern AFC_PATTERN = Pattern.compile("AFC BANCOLOMBIA - {2}" + NUMBER_REGEX);
+    private static final Pattern TOTAL_DEDUCTIONS_PATTERN = Pattern.compile("TOTAL DEDUCCIONES {2}" + NUMBER_REGEX);
+
+    private static final Pattern TOTAL_PAYMENT_PATTERN = Pattern.compile("TOTAL A PAGAR: {2}" + NUMBER_REGEX);
+
+    private static final CSVFormat CSV_FORMAT = CSVFormat.Builder
+            .create(TDF)
+            .setHeader(PayrollInfo.getHeaders())
             .build();
 
     @Parameters(paramLabel = "PDF_FILE", description = "one or more pdf files to analyze")
@@ -46,28 +68,62 @@ class payroll implements Callable<Integer> {
 
     private List<PayrollInfo> payrollInfoList;
 
+    @Value
+    @Builder
     static class PayrollInfo {
-        private final LocalDate date;
-        private final Number wage;
-        private final Number retention;
+        LocalDate date;
+        Number workHours;
+        Number wage;
 
-        public PayrollInfo(LocalDate date, Number wage, Number retention) {
-            this.date = date;
-            this.wage = wage;
-            this.retention = retention;
+        Number periodWage;
+        Number vacations;
+        Number totalWage;
+
+        Number retention;
+        Number health;
+        Number pension;
+        Number solidarity;
+        Number afc;
+        Number totalDeductions;
+
+        Number totalPayment;
+
+        public Object[] getValues() {
+            return new Object[]{
+                    date,
+                    workHours,
+                    wage,
+                    periodWage,
+                    vacations,
+                    totalWage,
+                    retention,
+                    health,
+                    pension,
+                    solidarity,
+                    afc,
+                    totalDeductions,
+                    totalPayment
+            };
         }
 
-        public LocalDate getDate() {
-            return date;
+        public static String[] getHeaders() {
+            return new String[]{
+                    "date",
+                    "workHours",
+                    "wage",
+                    "periodWage",
+                    "vacations",
+                    "totalWage",
+                    "retention",
+                    "health",
+                    "pension",
+                    "solidarity",
+                    "afc",
+                    "totalDeductions",
+                    "totalPayment"
+            };
         }
 
-        public Number getWage() {
-            return wage;
-        }
-
-        public Number getRetention() {
-            return retention;
-        }
     }
 
     @FunctionalInterface
@@ -86,13 +142,24 @@ class payroll implements Callable<Integer> {
         payrollInfoList = new ArrayList<>();
 
         for (File file : files) {
-            System.out.println("Parsing file = " + file);
-            String strippedText = pdfTextStripper.getText(PDDocument.load(file));
-            System.out.println("strippedText = " + strippedText);
-            Number retention = extractValue(strippedText, RETENTION_PATTERN, NUMBER_FORMAT::parse);
-            LocalDate date = extractValue(strippedText, DATE_PATTERN, v -> LocalDate.parse(v, DATE_TIME_FORMATTER));
-            Number wage = extractValue(strippedText, WAGE_PATTERN, NUMBER_FORMAT::parse);
-            payrollInfoList.add(new PayrollInfo(date, wage, retention));
+            try (PDDocument pdfDocument = PDDocument.load(file)) {
+                String strippedText = pdfTextStripper.getText(pdfDocument);
+                payrollInfoList.add(PayrollInfo.builder()
+                        .date(extractValue(strippedText, DATE_PATTERN, v -> LocalDate.parse(v, DATE_TIME_FORMATTER), FIRST_MATCHING_GROUP))
+                        .workHours(extractValue(strippedText, WORK_HOURS_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .wage(extractValue(strippedText, WAGE_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .periodWage(extractValue(strippedText, PERIOD_WAGE_PATTERN, NUMBER_FORMAT::parse, SECOND_MATCHING_GROUP))
+                        .vacations(extractValue(strippedText, VACATIONS_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .totalWage(extractValue(strippedText, TOTAL_WAGE_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .retention(extractValue(strippedText, RETENTION_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .health(extractValue(strippedText, HEALTH_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .pension(extractValue(strippedText, PENSION_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .solidarity(extractValue(strippedText, SOLIDARITY_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .afc(extractValue(strippedText, AFC_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .totalDeductions(extractValue(strippedText, TOTAL_DEDUCTIONS_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .totalPayment(extractValue(strippedText, TOTAL_PAYMENT_PATTERN, NUMBER_FORMAT::parse, FIRST_MATCHING_GROUP))
+                        .build());
+            }
         }
 
         printAsCSV();
@@ -104,10 +171,10 @@ class payroll implements Callable<Integer> {
         pdfTextStripper.setSortByPosition(true);
     }
 
-    private static <T> T extractValue(String text, Pattern retentionPattern, ValueParser<T> parser) throws Exception {
+    private static <T> T extractValue(String text, Pattern retentionPattern, ValueParser<T> parser, int group) throws Exception {
         Matcher matcher = retentionPattern.matcher(text);
         if (matcher.find()) {
-            return parser.parse(matcher.group(1));
+            return parser.parse(matcher.group(group));
         }
         return null;
     }
@@ -115,9 +182,7 @@ class payroll implements Callable<Integer> {
     private void printAsCSV() throws IOException {
         try (CSVPrinter printer = new CSVPrinter(System.out, CSV_FORMAT)) {
             for (PayrollInfo payrollInfo : payrollInfoList) {
-                printer.printRecord(payrollInfo.getDate(),
-                        payrollInfo.getWage(),
-                        payrollInfo.getRetention());
+                printer.printRecord(payrollInfo.getValues());
             }
         }
     }
